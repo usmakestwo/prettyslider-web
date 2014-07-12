1d25ba4782dee0f68d0809c1ec22a3d29072f58e
vazquez <2ricardo.vazquez@gmail.com>
2014-07-11 22:30:59 -0400

    finishing styling for slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
   var windowHeight = $(window).height(),
       shortWindowHeight = windowHeight - 60;
   $('#intro').css('min-height', shortWindowHeight);
-  $('#usage').css('min-height', windowHeight);
+  //$('#usage').css('min-height', windowHeight);
+  $('.fade-container').addClass('show');
 });
 
