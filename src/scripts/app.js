b33131947456b9a97ea18a6adfec6d79039a80a9
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-09-15 19:11:10 -0400

    removed old folder structure

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,5 @@
-var app = angular.module('app', ['prettySlider']);
+var app = angular.module('app', ['prettySlider'])
+
+app.controller('mainCtrl', function($scope) {
+	$scope.item = {cost: 0};
+});
